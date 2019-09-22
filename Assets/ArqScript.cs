c2c4f35d73ebdb5d347b8b568a2c82ae61f6b2eb
Amino <mr.amini0076@gmail.com>
2019-09-22 17:18:39 +0330

    init

---
+++ Assets/ArqScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArqScript : MonoBehaviour
+{
+    public Button btn;
+    public Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void BtnPressed()
+    {
+        anim.SetTrigger("play");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
